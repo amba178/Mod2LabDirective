243239c79eba7544acaad4da64d30fab41da7ca1
damu178 <salemamba@gmail.com>
2019-11-16 21:28:12 -0800

    add link function to update the directive

--- app/h3Directive.js
+++ app/h3Directive.js
@@ -1,11 +1,20 @@
 angular.module('app').directive('h3Directive', function() {
 
   return {
-    restrict: 'E',
+    restrict: 'EA',
+    replace: true,
     scope: {
       title: '@'
     },
     template: '<h3>{{title}}</h3>',
+    link: function(scope, element, attrs){
+      element.bind('mouseenter', function(){
+        element.css('background-color', 'red');
+      });
+      element.bind('mouseleave', function(){
+        element.css('background-color', 'silver');
+      })
+    }
   };
 
 });
--- app/personDirective.js
+++ app/personDirective.js
@@ -11,7 +11,8 @@ function person(){
             template: 'Pen Name: <input type="text" ng-model="person.name" class="form-control" />' +
                       'Pen Name: <input type="text" ng-model="person.penName" class="form-control" />' +
                       '<input type="button" ng-click="action()" value="Action" class="btn btn-primary"/>' + 
-                      '<pre>{{person | json}}</pre>'
+                      '<pre>{{person | json}}</pre>',
+            
 
   };
 };
